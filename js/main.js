403ed76262d93dc97523084bc873e35a8d7a259c
Andrew McCarthy <hi@andrevv.com>
2013-08-20 00:52:49 +0200

    silly syntax

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   funVerbs();
   instanames();
-  if("body").hasClass('feed') {
+  if($("body").hasClass('feed')) {
     $(window).scroll(function() {
       var hue = Math.round($(window).scrollTop() / 12);
       $("body").css({
