b8d73fdc5c2964256e307412e2dd4f306f6d5c70
Andrew McCarthy <hi@andrevv.com>
2013-10-28 22:30:54 +0100

    funverbs

--- js/main.js
+++ js/main.js
@@ -90,14 +90,17 @@ function splitLetters(el) {
 
 function funVerbs() {
   var verbs = new Array("hate","are disgusted by","loathe","despise","dislike");
+  var singular_verbs = new Array("hates","is disgusted by","loathes","despises","dislikes")
   $(".verb").each(function() {
-    var v = verbs[Math.floor(Math.random()*verbs.length)];
-    $(this).text(v);
-  });
-  $(".note-count").each(function() {
-    if($(this).text() === "1") {
-      $(this).next('.person').text("people");
+    var noteCount = $(this).prev('.note-count').find('span').text();
+    if (noteCount == 1) {
+      var v = singular_verbs[Math.floor(Math.random()*singular_verbs.length)];
+      $(this).prev('people').text('person');
+    } else {
+      var v = verbs[Math.floor(Math.random()*verbs.length)];
     }
+    
+    $(this).text(v);
   });
 }
 
